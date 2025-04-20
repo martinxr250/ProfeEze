e461b69e4ab274536b7223c7e863873ab68675c0
Martín <91429@sistemas.frc.utn.edu.ar>
2025-04-19 23:15:00 -0300

    1234567-LISTO

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -105,19 +105,6 @@ const Inicio = () => {
         </div>
       </div>
 
-      <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 animate-bounce">
-        <Link to="sobremi" spy={true} smooth={true} offset={-70} duration={500} className="text-white cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-8 w-8"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
-          </svg>
-        </Link>
-      </div>
     </section>
   )
 }
